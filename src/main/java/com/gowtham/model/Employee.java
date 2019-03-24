62473a4261bc57adbed1fa81d6cfd004814f2589
Gowtham Behara <gothsiddu@gmail.com>
2019-03-24 15:35:34 +0530

    Create Employee Model

---
+++ src/main/java/com/gowtham/model/Employee.java
@@ -0,0 +1,13 @@
+package com.gowtham.model;
+
+public class Employee {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
