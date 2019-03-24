679a2237332e31022ea8772e69fa651f62dc4250
Gowtham Behara <gothsiddu@gmail.com>
2019-03-24 16:39:21 +0530

    Skip throwing exception if no suitable bean is found

--- src/main/java/com/gowtham/service/annotation/EmployeeAutowiredByConstructorService.java
+++ src/main/java/com/gowtham/service/annotation/EmployeeAutowiredByConstructorService.java
@@ -1,11 +1,13 @@
 package com.gowtham.service.annotation;
 
 import com.gowtham.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 public class EmployeeAutowiredByConstructorService {
     private Employee employee;
 
+    @Autowired(required = false)
     public EmployeeAutowiredByConstructorService(@Qualifier("employee") Employee employee) {
         this.employee = employee;
     }
